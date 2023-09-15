a4d2f940e23aa1b948ed0dc0dc09d7da9a9c82eb
Krzysztof Modras <chrmod@chrmod.net>
2023-09-15 13:32:52 +0200

    Add ABP converter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import convertWithAdguard from './converters/adguard.js';
+import convertWithAbp from './converters/abp.js';
 
 const input = document.querySelector('#input textarea');
 const submitButton = document.querySelector('#input input[type=submit]');
-const output = document.querySelector('#output');
+const outputAdguard = document.querySelector('#output-adguard');
+const outputAbp = document.querySelector('#output-abp');
 
 submitButton.addEventListener('click', async (ev) => {
   ev.preventDefault();
-  const rules = input.value.split('n');
-  const convertedRules = await convertWithAdguard(rules);
-  output.innerHTML = JSON.stringify(convertedRules, null, 2);
+  const rules = input.value.split('\n');
+
+  const convertedRulesAdguard = await convertWithAdguard(rules);
+  const convertedRulesAbp = await convertWithAbp(rules);
+
+  outputAdguard.innerHTML = JSON.stringify(convertedRulesAdguard, null, 2);
+  outputAbp.innerHTML = JSON.stringify(convertedRulesAbp, null, 2);
 });
 
