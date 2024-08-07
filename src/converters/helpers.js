ghostery/urlfilter2dnr

commit 4067287a5671a0ff75bfd7773024b001e1e89eda

Normalise resource names (#16)
Co-authored-by: chrmod <chrmod@ghostery.com>
Co-authored-by: Philipp Claßen <philipp.classen@posteo.de>

src/converters/helpers.js
@@ -1,5 +1,57 @@
+import * as path from 'node:path'
+import redirects from '@adguard/scriptlets/dist/redirects.json' with { type: 'json' }
+
+export function generateResourcesMapping() {
+  const resourcesMapping = new Map();
+  const allowedResourceExtensions = [
+    'html',
+    'js',
+    'css',
+    'mp4',
+    'mp3',
+    'xml',
+    'txt',
+    'json',
+    'empty',
+  ];
+
+  function getPreferredResource(aliases) {
+    // ignore non-supported files and manually created uBO aliases by AdGuard
+    return aliases.find(alias => {
+      const extension = alias.split('.').pop();
+      return allowedResourceExtensions.includes(extension) && !alias.startsWith('ubo-');
+    });
+  }
+
+  for (const redirect of redirects) {
+    // Skip, in case of AdGuard-only resource
+    if (redirect.aliases === undefined) {
+      continue;
+    }
+
+    const preferredResourceName = getPreferredResource(redirect.aliases);
+
+    // Skip, in case of safe redirect resource name that's safe to use wasn't found
+    if (preferredResourceName === undefined) {
+      continue;
+    }
+
+    // Register to mapping
+    resourcesMapping.set(redirect.title, preferredResourceName);
+    for (const alias of redirect.aliases) {
+      if (alias !== preferredResourceName) {
+        resourcesMapping.set(alias, preferredResourceName);
+      }
+    }
+  }
+
+  return resourcesMapping;
+}
+
 export const DEFAULT_PARAM_MAPPING = {
   '3p': 'third-party',
+  'xhr': 'xmlhttprequest',
+  'frame': 'subdocument'
 };
 
 export function normalizeFilter(filter, { mapping = DEFAULT_PARAM_MAPPING } = {}) {
@@ -30,7 +82,9 @@ export function normalizeFilter(filter, { mapping = DEFAULT_PARAM_MAPPING } = {}
   return `${front}$${params.join(',')}`;
 }
 
-export function normalizeRule(rule) {
+export const DEFAULT_RESOURCE_MAPPING = generateResourcesMapping();
+
+export function normalizeRule(rule, { resourcesMapping = DEFAULT_RESOURCE_MAPPING } = {}) {
   if (!rule) {
     return;
   }
@@ -65,6 +119,16 @@ export function normalizeRule(rule) {
   if (newRule.condition && newRule.condition.domains) {
     newRule.condition.initiatorDomains = newRule.condition.domains;
     delete newRule.condition.domains;
+  }
+
+  if (newRule.action && newRule.action.type === 'redirect') {
+    const filename = path.basename(newRule.action.redirect.extensionPath);
+    const preferredFilename = resourcesMapping.get(filename);
+
+    if (preferredFilename !== undefined) {
+      newRule.action.redirect.extensionPath =
+        path.dirname(newRule.action.redirect.extensionPath) + '/' + preferredFilename;
+    }
   }
 
   return newRule;

test/unit/helpers.js
@@ -7,6 +7,9 @@ function normalize(rule) {
   if (!rule) {
     return undefined;
   }
+  if (rule.condition?.resourceTypes !== undefined) {
+    rule.condition.resourceTypes = rule.condition.resourceTypes.sort();
+  }
   delete rule.priority;
   delete rule.id;
   return rule;

.gitignore
@@ -1,3 +1,3 @@
 node_modules
 dist
-
+test-results

.tool-versions
@@ -1,1 +1,1 @@
-bun 1.0.1
+bun 1.1.17

test/unit/converters/helpers.spec.js
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "bun:test";
 
-import { normalizeFilter, normalizeRule } from "../../../src/converters/helpers.js";
+import { generateResourcesMapping, normalizeFilter, normalizeRule } from "../../../src/converters/helpers.js";
 
 describe("normalizeFilter", () => {
   it("format params", () => {
@@ -120,5 +120,40 @@ describe('normalizeRule', () => {
         excludedInitiatorDomains: ['test'],
       },
     });
+  });
+
+  it('replaces extensionPath respecting existing dirname', () => {
+    expect(normalizeRule({
+      action: {
+        type: 'redirect',
+        redirect: {
+          extensionPath: '/rule_resources/redirects/alias',
+        },
+      },
+    }, {
+      resourcesMapping: new Map([
+        [
+          'alias',
+          'test.js',
+        ],
+      ]),
+    })).toEqual({
+      action: {
+        type: 'redirect',
+        redirect: {
+          extensionPath: '/rule_resources/redirects/test.js',
+        },
+      },
+    });
+  });
+});
+
+describe('generateResourcesMapping', () => {
+  it('filters resources without file extension', () => {
+    const mapping = generateResourcesMapping();
+
+    for (const destination of mapping.values()) {
+      expect(destination.match(/\w+\.\w+|empty/)).not.toBe(null);
+    }
   });
 });

commit 41687f2daa65f7a40a2d96558d17a0d3ec6bc9c9

feat: remove node dependency (#17)
* feat: remove node dependency

* fix: correct scope

* fix: regressions in #16

- Handle image files
- Fix path handling

* fix: update function to respect nodejs spec

src/converters/helpers.js
@@ -1,27 +1,67 @@
-import * as path from 'node:path'
 import redirects from '@adguard/scriptlets/dist/redirects.json' with { type: 'json' }
+
+function getPathBasename(path) {
+  const lastIndex = path.lastIndexOf('/');
+  if (lastIndex === -1) {
+    return path;
+  }
+  return path.slice(lastIndex + 1);
+}
+
+function getPathDirname(path) {
+  const lastIndex = path.lastIndexOf('/');
+  if (lastIndex === -1) {
+    return '.';
+  }
+  return path.slice(0, lastIndex);
+}
+
+const allowedResourceExtensions = [
+  'html',
+  'js',
+  'css',
+  'mp4',
+  'mp3',
+  'xml',
+  'txt',
+  'json',
+  'png',
+  'gif',
+  'empty',
+];
+
+function getPreferredResource(aliases) {
+  // ignore non-supported files and manually created uBO aliases by AdGuard
+  return aliases.find(alias => {
+    const extension = alias.split('.').pop();
+    return extension !== undefined &&
+      allowedResourceExtensions.includes(extension) &&
+      !alias.startsWith('ubo-') &&
+      !alias.includes('-transparent');
+  });
+}
+
+function getFileExtensionByContentType(contentType) {
+  if (contentType.includes(';')) {
+    contentType = contentType.slice(0, contentType.indexOf(';'));
+  }
+  switch (contentType) {
+    case 'text/html':
+      return '.html';
+    case 'text/css':
+      return '.css';
+    case 'text/plain':
+    case 'application/javascript':
+      return '.js';
+    case 'application/json':
+      return '.json';
+  }
+
+  return '';
+}
 
 export function generateResourcesMapping() {
   const resourcesMapping = new Map();
-  const allowedResourceExtensions = [
-    'html',
-    'js',
-    'css',
-    'mp4',
-    'mp3',
-    'xml',
-    'txt',
-    'json',
-    'empty',
-  ];
-
-  function getPreferredResource(aliases) {
-    // ignore non-supported files and manually created uBO aliases by AdGuard
-    return aliases.find(alias => {
-      const extension = alias.split('.').pop();
-      return allowedResourceExtensions.includes(extension) && !alias.startsWith('ubo-');
-    });
-  }
 
   for (const redirect of redirects) {
     // Skip, in case of AdGuard-only resource
@@ -38,6 +78,7 @@ export function generateResourcesMapping() {
 
     // Register to mapping
     resourcesMapping.set(redirect.title, preferredResourceName);
+    resourcesMapping.set(redirect.title + getFileExtensionByContentType(redirect.contentType), preferredResourceName);
     for (const alias of redirect.aliases) {
       if (alias !== preferredResourceName) {
         resourcesMapping.set(alias, preferredResourceName);
@@ -122,12 +163,12 @@ export function normalizeRule(rule, { resourcesMapping = DEFAULT_RESOURCE_MAPPIN
   }
 
   if (newRule.action && newRule.action.type === 'redirect') {
-    const filename = path.basename(newRule.action.redirect.extensionPath);
+    const filename = getPathBasename(newRule.action.redirect.extensionPath);
     const preferredFilename = resourcesMapping.get(filename);
 
     if (preferredFilename !== undefined) {
       newRule.action.redirect.extensionPath =
-        path.dirname(newRule.action.redirect.extensionPath) + '/' + preferredFilename;
+        getPathDirname(newRule.action.redirect.extensionPath) + '/' + preferredFilename;
     }
   }
 