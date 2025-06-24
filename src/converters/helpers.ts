fd6cfd36b1dae8d4f4e54a7267620f0e1a290ebe
HoJeong Go <seia@outlook.kr>
2025-06-24 23:25:35 +0900

    refactor: omit `urlFilter` property in case of empty

--- src/converters/helpers.ts
+++ src/converters/helpers.ts
@@ -108,13 +108,16 @@ export function normalizeRule(rule: any, { resourcesPath = '', id }: { resources
   if (
     newRule.condition &&
     newRule.condition.urlFilter &&
-    newRule.condition.urlFilter.endsWith('*') &&
+    newRule.condition.urlFilter.endsWith('*')
+  ) {
     // Empty `RuleCondition.urlFilter` is not allowed
     // > *$xhr,removeparam=ad_config_id,domain=telequebec.tv
     // refs https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/declarativeNetRequest/RuleCondition#urlfilter
-    newRule.condition.urlFilter.length !== 1
-  ) {
-    newRule.condition.urlFilter = newRule.condition.urlFilter.slice(0, -1);
+    if (newRule.condition.urlFilter.length === 1) {
+      delete newRule.condition.urlFilter;
+    } else {
+      newRule.condition.urlFilter = newRule.condition.urlFilter.slice(0, -1);
+    }
   }
 
   if (rule.condition && rule.condition.isUrlFilterCaseSensitive !== true) {
