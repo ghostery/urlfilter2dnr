40993eca2ab197fe2896a0b698df34674487046b
Dominik Lubański <dominik.lubanski@gmail.com>
2026-03-25 10:43:56 +0100

    fix(exports): use export pattern for better tree-shaking (#162)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-import convertWithAdguard from './converters/adguard.js';
-import convertWithAbp from './converters/abp.js';
-
-export { convertWithAdguard, convertWithAbp };
+export { default as convertWithAdguard } from './converters/adguard.js';
+export { default as convertWithAbp } from './converters/abp.js';
